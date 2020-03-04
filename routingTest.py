599f26322dc446035f25a14220f06063ffedf737
MW <mw.alt.fox@gmail.com>
2020-03-03 17:39:14 -0800

    Add a Python script to test the new RoutingTable data structure

---
+++ routingTest.py
@@ -0,0 +1,39 @@
+from TestSim import TestSim
+
+def main():
+    # Initialize simulation
+    s = TestSim()
+    s.runTime(10)
+
+    # Load the network
+    s.loadTopo("split_loop.topo")
+    
+    # Add noise
+    s.loadNoise("no_noise.txt")
+    
+    # Turn on the sensors
+    s.bootAll()
+    
+    # Add channels
+#    s.addChannel(s.COMMAND_CHANNEL)
+#    s.addChannel(s.GENERAL_CHANNEL)
+    s.addChannel(s.ROUTING_CHANNEL)
+
+    s.runTime(20)
+
+    # Ping to start neighbor discovery
+    s.ping(1, 2, "Hello there")
+    s.runTime(10)
+    
+    # Print neighbors
+    for nodeID in range(1, s.numMote+1):
+        s.neighborDMP(nodeID)
+        s.runTime(10)
+
+    # Print routing tables
+    for nodeID in range(1, s.numMote+1):
+        s.routeDMP(nodeID)
+        s.runTime(10)
+
+if __name__ == '__main__':
+    main()
